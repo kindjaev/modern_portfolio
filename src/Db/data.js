36673b9e27bf10940812f65ef1568d748271afc5
Dony Dexon <kindjaev080592@gmail.com>
2023-06-07 23:41:35 +1000

    updated some project info

--- src/Db/data.js
+++ src/Db/data.js
@@ -1,17 +1,66 @@
-import IMG1 from "../assets/yelpcamp.png"
-import IMG2 from "../assets/ecommerce.png"
-import IMG3 from "../assets/workout_body.png"
-import IMG4 from "../assets/tenzies.png"
-import IMG5 from "../assets/portfolio.png"
-import IMG6 from "../assets/chat_app.png"
-import IMG7 from "../assets/fitness.png"
+import IMG1 from "../assets/yelpcamp.png";
+import IMG2 from "../assets/ecommerce.png";
+import IMG3 from "../assets/workout_body.png";
+import IMG4 from "../assets/tenzies.png";
+import IMG5 from "../assets/portfolio.png";
+import IMG6 from "../assets/whale_ai.png";
+import IMG7 from "../assets/fitness.png";
 
 export const data = [
-    {id: 0, img: IMG1, linkCode: "https://github.com/kindjaev/YelpCamp", linkCodeName: "GitHub", linkLive: "https://yelp-camp-va84.onrender.com", title: "YelpCamp"},
-    {id: 1, img: IMG2, linkCode: "https://github.com/kindjaev/ecommerce_sanity_stripe", linkCodeName: "GitHub", linkLive: "https://ecommerce-sanity-stripe-kindjaev.vercel.app", title: "Ecommerce Shop"},
-    {id: 2, img: IMG3, linkCode: "https://github.com/kindjaev/MERN-frontend", linkCodeName: "GitHub", linkLive: "https://mern-frontend-kindjaev.vercel.app/login", title: "Workout Body"},
-    {id: 3, img: IMG4, linkCode: "https://github.com/kindjaev/tenzies", linkCodeName: "GitHub", linkLive: "https://tenzies-dony-kindjaev.vercel.app/", title: "Tenzies"},
-    {id: 4, img: IMG5, linkCode: "https://github.com/kindjaev/modern_portfolio", linkCodeName: "GitHub", linkLive: "https://modern-portfolio-kindjaev.vercel.app", title: "Modern Portfolio"},
-    {id: 5, img: IMG6, linkCode: "https://github.com/kindjaev/chat-app", linkCodeName: "GitHub", linkLive: "https://chat-app-kindjaev.vercel.app", title: "Chat App"},
-    {id: 6, img: IMG7, linkCode: "https://github.com/kindjaev/frontend-fitness-auth", linkCodeName: "GitHub", linkLive: "https://frontend-fitness-auth.vercel.app", title: "Fitness App"},
-  ]
+  {
+    id: 0,
+    img: IMG1,
+    linkCode: "https://github.com/kindjaev/YelpCamp",
+    linkCodeName: "GitHub",
+    linkLive: "https://yelp-camp-va84.onrender.com",
+    title: "YelpCamp",
+  },
+  {
+    id: 1,
+    img: IMG2,
+    linkCode: "https://github.com/kindjaev/ecommerce_sanity_stripe",
+    linkCodeName: "GitHub",
+    linkLive: "https://ecommerce-sanity-stripe-kindjaev.vercel.app",
+    title: "Ecommerce Shop",
+  },
+  {
+    id: 2,
+    img: IMG3,
+    linkCode: "https://github.com/kindjaev/MERN-frontend",
+    linkCodeName: "GitHub",
+    linkLive: "https://mern-frontend-kindjaev.vercel.app/login",
+    title: "Workout Body",
+  },
+  {
+    id: 3,
+    img: IMG4,
+    linkCode: "https://github.com/kindjaev/tenzies",
+    linkCodeName: "GitHub",
+    linkLive: "https://tenzies-dony-kindjaev.vercel.app/",
+    title: "Tenzies",
+  },
+  {
+    id: 4,
+    img: IMG5,
+    linkCode: "https://github.com/kindjaev/modern_portfolio",
+    linkCodeName: "GitHub",
+    linkLive: "https://modern-portfolio-kindjaev.vercel.app",
+    title: "Modern Portfolio",
+  },
+  {
+    id: 5,
+    img: IMG6,
+    linkCode: "https://github.com/kindjaev/chatGPT",
+    linkCodeName: "GitHub",
+    linkLive: "https://whale-ai-seven.vercel.app",
+    title: "WhaleAi",
+  },
+  {
+    id: 6,
+    img: IMG7,
+    linkCode: "https://github.com/kindjaev/frontend-fitness-auth",
+    linkCodeName: "GitHub",
+    linkLive: "https://frontend-fitness-auth.vercel.app",
+    title: "Fitness App",
+  },
+];
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import {BsFillPatchCheckFill} from "react-icons/bs"
-import "./experince.css"
-
+import React from "react";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 function Experience() {
-
   const frontendData = [
-    {id: 0, title: "HTML", text: "Experienced"},
-    {id: 1, title: "React", text: "Experinced"},
-    {id: 2, title: "CSS", text: "Experinced"},
-    {id: 3, title: "JS", text: "Experinced"},
-    {id: 4, title: "Bootstrap", text: "Basic"},
-    {id: 5, title: "Tailwind", text: "Basic"},
-  ]
+    { id: 0, title: "HTML", text: "Experienced" },
+    { id: 1, title: "React", text: "Experinced" },
+    { id: 2, title: "CSS", text: "Experinced" },
+    { id: 3, title: "JS", text: "Experinced" },
+    { id: 4, title: "Bootstrap", text: "Basic" },
+    { id: 5, title: "Tailwind", text: "Basic" },
+  ];
   const backendData = [
-    {id: 0, title: "NodeJS", text: "basic"},
-    {id: 1, title: "MongoDB", text: "basic"},
-    {id: 2, title: "PHP", text: "basic"},
-  ]
+    { id: 0, title: "NodeJS", text: "basic" },
+    { id: 1, title: "MongoDB", text: "basic" },
+    { id: 2, title: "PHP", text: "basic" },
+  ];
   return (
-    <section id='experince'>
+    <section id="experince">
       <h2>What Skills I have</h2>
       <h5>My Experince</h5>
 
@@ -27,35 +25,35 @@ function Experience() {
         <div className="frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
-              {frontendData.map(el => 
-                <div className="experience_details" key={el.id}>
-                  <BsFillPatchCheckFill className="experience_icons"/>
-                  <div>
-                    <h4>{el.title}</h4>
-                    <small className='text-light'>{el.text}</small>
-                  </div>
+            {frontendData.map((el) => (
+              <div className="experience_details" key={el.id}>
+                <BsFillPatchCheckFill className="experience_icons" />
+                <div>
+                  <h4>{el.title}</h4>
+                  <small className="text-light">{el.text}</small>
                 </div>
-              )}
+              </div>
+            ))}
           </div>
         </div>
         {/* ========== BACKEND ========== */}
         <div className="backend">
           <h3>Backend Development</h3>
           <div className="experience_content">
-            {backendData.map(el => 
+            {backendData.map((el) => (
               <div className="experience_details" key={el.id}>
-                <BsFillPatchCheckFill className="experience_icons"/>
+                <BsFillPatchCheckFill className="experience_icons" />
                 <div>
                   <h4>{el.title}</h4>
-                  <small className='text-light'>{el.text}</small>
+                  <small className="text-light">{el.text}</small>
                 </div>
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
